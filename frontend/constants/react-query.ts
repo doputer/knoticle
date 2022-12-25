8afbb4f811af1926c64d974ef1ce34b08c7c678e
김도현 <swputer@gmail.com>
2022-12-26 04:52:56 +0900

    feat: React Query 옵션 추가 - #14

---
+++ frontend/constants/react-query.ts
@@ -0,0 +1,8 @@
+// eslint-disable-next-line import/prefer-default-export
+export const DISABLE_REFETCH_OPTIONS = {
+  refetchOnWindowFocus: false,
+  refetchInterval: false,
+  refetchOnMount: false,
+  refetchOnReconnect: false,
+  refetchIntervalInBackground: false,
+} as const;
