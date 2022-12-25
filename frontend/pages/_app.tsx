2851835768199012f380ea3fa6d6594e1b2151bd
김도현 <swputer@gmail.com>
2022-12-26 04:50:09 +0900

    chore: React Query 라이브러리 추가 - #14

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 
 import type { ReactElement, ReactNode } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { ToastContainer } from 'react-toastify';
 
 import { RecoilRoot } from 'recoil';
@@ -11,7 +12,6 @@ import CheckSignInStatus from '@components/auth/CheckSignInStatus';
 import GlobalModal from '@components/common/GlobalModal';
 import GlobalStyle from '@styles/GlobalStyle';
 import responsive from '@styles/responsive';
-
 import 'react-toastify/dist/ReactToastify.css';
 import '@styles/font.css';
 
@@ -24,18 +24,21 @@ type AppPropsWithLayout = AppProps & {
 };
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const queryClient = new QueryClient();
   const getLayout = Component.getLayout ?? ((page) => page);
 
   return (
-    <RecoilRoot>
-      <CheckSignInStatus>
-        <GlobalStyle />
-        <ThemeProvider theme={responsive}>
-          {getLayout(<Component {...pageProps} />)}
-          <ToastContainer limit={3} />
-          <GlobalModal />
-        </ThemeProvider>
-      </CheckSignInStatus>
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <CheckSignInStatus>
+          <GlobalStyle />
+          <ThemeProvider theme={responsive}>
+            {getLayout(<Component {...pageProps} />)}
+            <ToastContainer limit={3} />
+            <GlobalModal />
+          </ThemeProvider>
+        </CheckSignInStatus>
+      </RecoilRoot>
+    </QueryClientProvider>
   );
 }
