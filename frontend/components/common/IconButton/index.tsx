802ff2a145849b369beec19b7e3111d031f91b3d
김도현 <swputer@gmail.com>
2022-12-25 21:42:57 +0900

    feat: IconButton 컴포넌트 추가

---
+++ frontend/components/common/IconButton/index.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image';
+
+interface IconButtonProps {
+  src: string;
+  alt: string;
+  visible?: boolean;
+  onClick: () => void;
+}
+
+export default function IconButton({ src, alt, visible, onClick }: IconButtonProps) {
+  return (
+    <button type="button" onClick={onClick} style={{ visibility: visible ? 'visible' : 'hidden' }}>
+      <Image src={src} alt={alt} />
+    </button>
+  );
+}
+
+IconButton.defaultProps = {
+  visible: true,
+};
