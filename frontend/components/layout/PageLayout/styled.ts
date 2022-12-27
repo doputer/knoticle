9a14a42a13fc525a80554ca794fec9f35c4e360b
김도현 <swputer@gmail.com>
2022-12-28 06:08:57 +0900

    design: 레이아웃 스타일 수정

--- frontend/components/layout/PageLayout/styled.ts
+++ frontend/components/layout/PageLayout/styled.ts
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
 
+import { FlexColumnAlignCenter } from '@styles/layout';
+
 // eslint-disable-next-line import/prefer-default-export
-export const PageInnerLarge = styled.div`
+export const PageLarge = styled(FlexColumnAlignCenter)`
   margin: 0 auto;
   max-width: 1500px;
 `;
