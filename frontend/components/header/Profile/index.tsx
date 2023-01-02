c06876fdc31708e5b16cc8e2992fb26c82842954
김도현 <swputer@gmail.com>
2023-01-02 19:35:16 +0900

    feat: Profile 컴포넌트 드롭다운 추가 - #29

--- frontend/components/header/Profile/index.tsx
+++ frontend/components/header/Profile/index.tsx
@@ -1,26 +1,53 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
+import { useEffect, useState } from 'react';
+import { useMutation } from 'react-query';
+
+import { signOutApi } from '@apis/authApi';
 import useUser from '@hooks/useUser';
 
-import { ProfileContainer, ProfileImage } from './styled';
+import { Dropdown, DropdownItem, ProfileContainer, ProfileImage } from './styled';
 
 export default function Profile() {
-  const { signInUser } = useUser();
+  const router = useRouter();
+  const { signInUser, clearUser } = useUser();
+  const { mutate: signOut } = useMutation(signOutApi, {
+    onSuccess: () => {
+      clearUser();
+
+      router.reload();
+    },
+  });
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+
+  const handleProfileClick = (event: React.MouseEvent) => {
+    event.stopPropagation();
+    setDropdownVisible(!dropdownVisible);
+  };
+
+  const handleDocumentClick = () => setDropdownVisible(false);
+
+  useEffect(() => {
+    document.addEventListener('click', handleDocumentClick);
+
+    return () => document.removeEventListener('click', handleDocumentClick);
+  }, []);
 
   return (
-    <ProfileContainer>
-      <Link href={`/@${signInUser.nickname}`}>
-        <ProfileImage>
-          <Image
-            src={signInUser.profile_image}
-            alt="Profile Image"
-            width={32}
-            height={32}
-            priority
-          />
-        </ProfileImage>
-      </Link>
+    <ProfileContainer onClick={handleProfileClick}>
+      <ProfileImage>
+        <Image src={signInUser.profile_image} alt="Profile Image" width={32} height={32} />
+      </ProfileImage>
+      {dropdownVisible && (
+        <Dropdown>
+          <Link href={`/@${signInUser.nickname}`}>
+            <DropdownItem>내 서재</DropdownItem>
+          </Link>
+          <DropdownItem onClick={() => signOut()}>로그아웃</DropdownItem>
+        </Dropdown>
+      )}
     </ProfileContainer>
   );
 }
--- frontend/components/header/Profile/styled.ts
+++ frontend/components/header/Profile/styled.ts
@@ -1,6 +1,11 @@
 import styled from 'styled-components';
 
-export const ProfileContainer = styled.div``;
+import { TextMedium } from '@styles/common';
+
+export const ProfileContainer = styled.div`
+  position: relative;
+  cursor: pointer;
+`;
 
 export const ProfileImage = styled.div`
   width: 32px;
@@ -8,8 +13,29 @@ export const ProfileImage = styled.div`
   border: 1px solid var(--grey-01-color);
   border-radius: 50%;
   overflow: hidden;
+  box-sizing: border-box;
 
   img {
     object-fit: cover;
+  }
+`;
+
+export const Dropdown = styled.div`
+  background-color: var(--white-color);
+  position: absolute;
+  bottom: -8px;
+  right: 0;
+  transform: translate(0, 100%);
+  box-shadow: rgb(0 0 0 / 10%) 0px 0px 8px;
+`;
+
+export const DropdownItem = styled(TextMedium)`
+  padding: 16px 0;
+  width: 128px;
+  text-align: center;
+
+  :hover {
+    color: var(--primary-color);
+    background-color: var(--grey-03-color);
   }
 `;
