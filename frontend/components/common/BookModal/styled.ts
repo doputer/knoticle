9f41b3bf8fb59c777ec275e07c816cbfa0c6e09e
김도현 <swputer@gmail.com>
2023-01-16 05:04:52 +0900

    design: BookModal 컴포넌트 디자인 수정 - #36

--- frontend/components/common/BookModal/styled.ts
+++ frontend/components/common/BookModal/styled.ts
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
 
+import { TextSmall } from '@styles/common';
 import { FlexColumn } from '@styles/layout';
 
 export const SelectBookModalContainer = styled(FlexColumn)`
   height: 100%;
-  gap: 32px;
+  gap: 16px;
 `;
 
 export const SelectWrapper = styled.div`
@@ -27,4 +28,8 @@ export const SelectItem = styled.div`
     background-color: var(--grey-03-color);
     cursor: pointer;
   }
+`;
+
+export const SelectLabel = styled(TextSmall)`
+  text-align: center;
 `;
